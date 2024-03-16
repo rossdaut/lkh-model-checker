1922131819d241fb54c754d281ea287941629046
Joel D'Autilio <joeldautilio@gmail.com>
2024-03-16 12:38:01 -0300

    define satisfiesAll method
    Co-authored-by: Pablo Rossi <pablorossi2001@gmail.com>

--- src/main/java/lkh/lts/State.java
+++ src/main/java/lkh/lts/State.java
@@ -4,11 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode
 public class State {
-  HashSet<Proposition> propositions;
+  Set<Proposition> propositions;
+
+  /**
+   * Return whether the given propositions hold
+   * @param propositions a collection of propositions
+   * @return true iff all propositions hold
+   */
+  boolean satisfiesAll(Collection<Proposition> propositions) {
+    return this.propositions.containsAll(propositions);
+  }
 }
