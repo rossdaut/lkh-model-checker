57859bb33d91f7b8583f6e6b4d09e4b697181914
Joel D'Autilio <joeldautilio@gmail.com>
2024-06-08 14:59:19 -0300

    Fix tabs

--- src/main/java/lkh/automata/DeterministicAutomaton.java
+++ src/main/java/lkh/automata/DeterministicAutomaton.java
@@ -6,22 +6,22 @@ import java.util.Map;
 import java.util.Set;
 
 public class DeterministicAutomaton<State, Symbol> extends NonDeterministicAutomaton<State, Symbol> {
-    @Override
-    public boolean addEmptyTransition(State source, State target) {
-        throw new UnsupportedOperationException("Cannot add an empty transition to a FDA");
-    }
+  @Override
+  public boolean addEmptyTransition(State source, State target) {
+    throw new UnsupportedOperationException("Cannot add an empty transition to a FDA");
+  }
 
-    @Override
-    public boolean addTransition(State source, State target, Symbol symbol) {
-        removeTransition(source, symbol);
-        return super.addTransition(source, target, symbol);
-    }
+  @Override
+  public boolean addTransition(State source, State target, Symbol symbol) {
+    removeTransition(source, symbol);
+    return super.addTransition(source, target, symbol);
+  }
 
-    private boolean removeTransition(State source, Symbol symbol) {
-        Map<Symbol, Set<State>> sourceMap = transitionsMap.get(source);
-        if (sourceMap == null) return false;
+  private boolean removeTransition(State source, Symbol symbol) {
+    Map<Symbol, Set<State>> sourceMap = transitionsMap.get(source);
+    if (sourceMap == null) return false;
 
-        sourceMap.remove(symbol);
-        return true;
-    }
+    sourceMap.remove(symbol);
+    return true;
+  }
 }
