56c9372f8efd2683284dc745c3f4feaa184f6331
Joel D'Autilio <joeldautilio@gmail.com>
2024-08-08 11:42:45 -0300

    Refactor: reorganize and comment
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- src/main/java/lkh/lts/LTS.java
+++ src/main/java/lkh/lts/LTS.java
@@ -6,9 +6,27 @@ import java.util.Set;
 public interface LTS {
   void addState(State state);
 
+  /**
+   * Add a transition. If the states don't exist, add them too.
+   * @param src a non-null source state
+   * @param action a non-null action
+   * @param dest a non-null target state
+   * @throws NullPointerException if any of the arguments is null
+   */
   void addTransition(State src, Action action, State dest);
 
+  /**
+   * Get the set of states of LTS
+   * @return the set of states
+   */
   Set<State> getStates();
+
+  /**
+   * Return whether the given state exists in the LTS
+   * @param state the state to check
+   * @return true if the state exists in the LTS, false otherwise
+   */
+  boolean containsState(State state);
 
   /**
    * Return the set of states reachable from the given source by consuming the given action
@@ -24,6 +42,4 @@ public interface LTS {
    * @return a set of states
    */
   Set<State> evaluate(Collection<Proposition> propositions);
-
-  boolean containsState(State state);
 }
