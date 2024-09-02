07fe537f2c815caf69d91c203974b592e83ac1ff
PabloRoss7 <pablorossi2001@gmail.com>
2024-09-02 12:43:45 -0300

    Change tests for a more complex LTS.
    Co-authored-by: Joel D'Autilio <joeldautilio@gmail.com>

--- src/test/java/lkh/modelchecker/ModelCheckerKhTest.java
+++ src/test/java/lkh/modelchecker/ModelCheckerKhTest.java
@@ -14,25 +14,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ModelCheckerKhTest {
   static ModelChecker<Integer, String> modelChecker;
+
   @BeforeEach
   void setUp() {
-    // [p] -a-> [q] -b-> [r]    [p] -a-> [q]
     LTS<Integer, String> lts = new HashMapLTS<>();
-    lts.addState(0, Set.of("p"));
-    lts.addState(1, Set.of("q"));
-    lts.addState(2, Set.of("r"));
-    lts.addState(3, Set.of("p"));
-    lts.addState(4, Set.of("q"));
+    lts.addState(0, Set.of("p", "q", "s"));
+    lts.addState(1, Set.of("q", "r"));
+    lts.addState(2, Set.of("p", "s"));
+    lts.addState(3, Set.of("t"));
+
+    lts.addState(4, Set.of("p", "q"));
+    lts.addState(5, Set.of("s"));
+    lts.addState(6, Set.of("t"));
 
     lts.addTransition(0, 1, "a");
     lts.addTransition(1, 2, "b");
-    lts.addTransition(3, 4, "a");
+    lts.addTransition(1, 3, "c");
+    lts.addTransition(3, 0, "b");
+
+    lts.addTransition(4, 5, "a");
+    lts.addTransition(5, 6, "b");
 
     modelChecker = new ModelChecker<>(lts, 0);
   }
 
   @ParameterizedTest
-  @CsvSource({ "'kh(p, q)', true", "'kh(p, r)', false"}) //kh(p, s), false
+  @CsvSource({ "'kh(p and q, s or t)', true", "'kh(p, p)', true", "'kh(u,p)', true", "'kh(q and r, r)', true"})
   void testKh(String expressionString, boolean expected) throws ParseException {
     Expression expression = Expression.of(expressionString);
 
